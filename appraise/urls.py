49f602e0deb4fb0c7cbf55f3827c9cb6737d83c2
Fred (feedoo) Blain <f.blain@sheffield.ac.uk>
2017-03-17 18:12:49 +0000

    views/urls: updated to meteval app

--- appraise/urls.py
+++ appraise/urls.py
@@ -25,18 +25,18 @@ urlpatterns = patterns('appraise.views',
   (r'^{0}admin/'.format(DEPLOYMENT_PREFIX), include(admin.site.urls)),
 )
 
-# Patterns for "wmt16" app.
-urlpatterns += patterns('appraise.wmt16.views',
-  (r'^{0}wmt16/$'.format(DEPLOYMENT_PREFIX), 'overview'),
-  (r'^{0}wmt16/(?P<hit_id>[a-f0-9]{{8}})/'.format(DEPLOYMENT_PREFIX), 'hit_handler'),
-  (r'^{0}wmt16/status/$'.format(DEPLOYMENT_PREFIX), 'status'),
-  (r'^{0}wmt16/update-status/(?P<key>(global_stats|language_pair_stats|group_stats|user_stats|clusters))?/?$'.format(DEPLOYMENT_PREFIX), 'update_status'),
-  (r'^{0}wmt16/update-ranking/$'.format(DEPLOYMENT_PREFIX), 'update_ranking'),
-  (r'^{0}wmt16/signup/$'.format(DEPLOYMENT_PREFIX), 'signup'),
-  (r'^{0}wmt16/profile/$'.format(DEPLOYMENT_PREFIX), 'profile_update'),
-  (r'^{0}wmt16/export-to-pairwise-csv/(?P<token>[^/]+)/(?P<project>[^/]+)/$'.format(DEPLOYMENT_PREFIX), 'export_to_pairwise_csv'),
-  (r'^{0}wmt16/export-to-ranking-csv/(?P<token>[^/]+)/(?P<project>[^/]+)/$'.format(DEPLOYMENT_PREFIX), 'export_to_ranking_csv'),
-  (r'^{0}wmt16/export-to-ranking-xml/(?P<token>[^/]+)/(?P<project>[^/]+)/$'.format(DEPLOYMENT_PREFIX), 'export_to_ranking_xml'),
+# Patterns for "meteval" app.
+urlpatterns += patterns('appraise.meteval.views',
+  (r'^{0}meteval/$'.format(DEPLOYMENT_PREFIX), 'overview'),
+  (r'^{0}meteval/(?P<hit_id>[a-f0-9]{{8}})/'.format(DEPLOYMENT_PREFIX), 'hit_handler'),
+  (r'^{0}meteval/status/$'.format(DEPLOYMENT_PREFIX), 'status'),
+  (r'^{0}meteval/update-status/(?P<key>(global_stats|language_pair_stats|group_stats|user_stats|clusters))?/?$'.format(DEPLOYMENT_PREFIX), 'update_status'),
+  (r'^{0}meteval/update-ranking/$'.format(DEPLOYMENT_PREFIX), 'update_ranking'),
+  (r'^{0}meteval/signup/$'.format(DEPLOYMENT_PREFIX), 'signup'),
+  (r'^{0}meteval/profile/$'.format(DEPLOYMENT_PREFIX), 'profile_update'),
+  (r'^{0}meteval/export-to-pairwise-csv/(?P<token>[^/]+)/(?P<project>[^/]+)/$'.format(DEPLOYMENT_PREFIX), 'export_to_pairwise_csv'),
+  (r'^{0}meteval/export-to-ranking-csv/(?P<token>[^/]+)/(?P<project>[^/]+)/$'.format(DEPLOYMENT_PREFIX), 'export_to_ranking_csv'),
+  (r'^{0}meteval/export-to-ranking-xml/(?P<token>[^/]+)/(?P<project>[^/]+)/$'.format(DEPLOYMENT_PREFIX), 'export_to_ranking_xml'),
 )
 
 if DEBUG:
